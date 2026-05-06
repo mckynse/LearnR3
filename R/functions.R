47c93d316c2345f3d6e334d3c48466985af92008
Cecilie Kynding Kristensen <c.kynding@hotmail.com>
2026-05-06 15:44:29 +0200

    moving functions from learning.qmd to R/functions.R and updating cleaning.qmd

--- R/functions.R
+++ R/functions.R
@@ -15,6 +15,42 @@ read <- function(file_path, max_rows = 10) {
   return(data)
 }
 
+#' Extract participant ID from file_path_id
+#'
+#' @param data
+#'
+#' @returns data with ID
+#'
+get_participant_id <- function(data) {
+  data_with_id <- data |>
+    dplyr::mutate(
+      id = stringr::str_extract(
+        file_path_id,
+        pattern = "/stress/[:alnum:]{2}/"
+      ) |>
+        stringr::str_remove("/stress/") |>
+        stringr::str_remove("/"),
+      .before = file_path_id
+    ) |>
+    dplyr::select(-file_path_id)
+
+  return(data_with_id)
+}
+
+#' Reading all files in a measured parameter (directory) and binding them by file_path_id
+#'
+#' @param filename
+#'
+#' @returns all same type parameter data in one data file
+#'
+read_all <- function(filename) {
+  data <- here::here("data-raw/nurses-stress") |>
+    fs::dir_ls(regexp = filename, recurse = TRUE) |>
+    purrr::map(read) |> # NO BRACKETS!
+    purrr::list_rbind(names_to = "file_path_id")
+  return(data)
+}
+
 #' Summarising by groups
 #'
 #' @param data
